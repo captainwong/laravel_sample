b3472aeecdfa563ab20d6130a98cefe76699ef3c
vagrant-homestead <qtk.jack@gmail.com>
2018-09-19 14:44:55 +0000

    add account activations

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -6,13 +6,14 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Models\User;
 use Auth;
+use Mail;
 
 class UsersController extends Controller
 {
 
     public function __construct(){
         $this->middleware('auth', [
-            'except' => ['show', 'create', 'store', 'index']
+            'except' => ['show', 'create', 'store', 'index', 'confirmEmail']
         ]);
 
         $this->middleware('guest', [
@@ -41,9 +42,33 @@ class UsersController extends Controller
             'password' => bcrypt($request->password)
         ]); 
 
-        Auth::login($user);
-        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+        $this->sendEmailConfirmationTo($user);
+        session()->flash('success', '验证邮件已发送到您的注册邮箱，请注意查收。');
 
+        return redirect('/');
+    }
+
+    protected function sendEmailConfirmationTo(User $user){
+        $view = 'emails.confirm';
+        $data = compact('user');
+        $from = 'qtk.jack@gmail.com';
+        $name = "Jack";
+        $to = $user->email;
+        $subject = '感谢注册Sample应用！请确认您的邮箱。';
+
+        Mail::send($view, $data, function($message) use($from, $name, $to, $subject){
+            $message->from($from, $name)->to($to)->subject($subject);
+        });
+    }
+
+    public function confirmEmail($token){
+        $user = User::where('activation_token', $token)->firstOrFail();
+        $user->activated = true;
+        $user->activation_token = null;
+        $user->save();
+
+        Auth::login($user);
+        session()->flash('success', '恭喜你，激活成功！');
         return redirect()->route('users.show', [$user]);
     }
 
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -18,8 +18,9 @@ class UsersTableSeeder extends Seeder
         $user = User::find(1);
         $user->name= 'Jack';
         $user->email = 'qtk.jack@gmail.com';
-        $user->password = bcrypt('lalala');
+        $user->password = bcrypt('aaaaaa');
         $user->is_admin = true;
+        $user->activated = true;
         $user->save();
     }
 }
